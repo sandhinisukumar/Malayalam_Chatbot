0ed9c70c15905a1367d31b18074f8a03e96ed715
reshma <reshma12393@gmail.com>
2018-12-03 18:20:34 +0530

    credits added for chatterbot library in readme

--- README.md
+++ README.md
@@ -1,11 +1,60 @@
-## A Malayalam Chatbot
+# A Malayalam Chatbot
+
 A chatbot is a conversational agent which interacts with humans via natural languages.
 Text as well as speech, is used as the input to these systems. Proposes a Malayalam chatbot based on a language-independent natural language processing library with a learning mechanism. The chatbot is a retrieval based model which can converses in Malayalam. Malayalam is a Dravidian language talked over the Indian state of Kerala.
 Machine learning as well as NLP (Natural Language Processing) approaches are used to analyze user queries and generate responses.
 
+## Getting Started
 
-## How to run program
-open terminal__
-python training.py__
-python chat.py__
-open url in the browser
+### Prerequisites
+1. Python 2.7 
+
+2. Flask 0.12
+
+3. Python chatterbot library is required for running the Malayalam chatterbot project.
+
+Installation details:
+Installing from PyPi:
+```
+pip install chatterbot
+```
+
+Installing from GitHub:
+```
+pip install git+git://github.com/gunthercox/ChatterBot.git@master
+```
+
+### Installing
+
+Follow the steps to run the Malayalam chatterbot:
+
+Open terminal
+
+Perform Training on Malayalam data using the following command:
+
+```
+python training.py
+```
+
+And run chat.py :
+
+```
+python chat.py
+```
+
+open the url from terminal in the browser
+
+## Built With
+
+* [ChatterBot](https://github.com/gunthercox/ChatterBot) Copyright (c) 2016 - 2018, Gunther Cox , All rights reserved.- The chatbot framework used
+
+## Authors
+
+* **Sandhini S** - *Initial work* - [Sandhinisukumar](https://gitlab.com/Sandhinisukumar)
+
+## License
+
+This project is licensed under the GNU - GPL v3 - see the (https://www.gnu.org/licenses/gpl-3.0.en.html) file for details
+
+
+
--- chat.py
+++ chat.py
@@ -16,12 +16,12 @@ import logging
 # Create a new instance of a ChatBot
 chatbot = ChatBot(
         "TourismBot",  # These are the adapters that are required to build a chatbot
-	storage_adapter='chatterbot.storage.SQLStorageAdapter',
-	output_adapter='chatterbot.output.OutputAdapter',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    output_adapter='chatterbot.output.OutputAdapter',
         output_format="text",   # The output format is "text"
-	statement_comparison_function=levenshtein_distance,	# The sentence comparison is calculated by using Levenshtein distance function
-	# These are the logic adapters that are required for normal operation        
-	logic_adapters=[
+    statement_comparison_function=levenshtein_distance,    # The sentence comparison is calculated by using Levenshtein distance function
+    # These are the logic adapters that are required for normal operation        
+    logic_adapters=[
             {
                 'import_path': 'chatterbot.logic.BestMatch' 
             },
@@ -30,8 +30,8 @@ chatbot = ChatBot(
                 'threshold': 0.5,                
                 'default_response': 'വെരെ ഏതെങ്കിലും അറിയാനുണ്ടോ?'
             }
-	],
-	
+    ],
+    
       
     )
 @app.route("/")
@@ -39,14 +39,14 @@ def home():
     return render_template("index3.html")
 @app.route("/get")
 def get_bot_response():
-	while True:		# The following loop will execute each time the user enters input
-		try:
-    			response = request.args.get('msg')
-  			return str(chatbot.get_response(response))		# Select a response to the input statement
+    while True:        # The following loop will execute each time the user enters input
+        try:
+            response = request.args.get('msg')
+            return str(chatbot.get_response(response))        # Select a response to the input statement
 
 
-    		except(KeyboardInterrupt, EOFError, SystemExit):		# Press ctrl-c or ctrl-d on the keyboard to exit
-    			print('\nനന്ദി!\n')
-        		break
+        except(KeyboardInterrupt, EOFError, SystemExit):        # Press ctrl-c or ctrl-d on the keyboard to exit
+            print('\nനന്ദി!\n')
+            break
 if __name__ == "__main__":
     app.run()
